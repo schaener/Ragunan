e44a65e3350498e50af0136059dd0ad8746e0abc
schaener <aoktafiando@gmail.com>
2020-03-16 14:35:10 +0700

    latest

---
+++ app/src/main/java/ragunan/javafirst/ui/Main/Transaction/Detail/TransactionDetail.java
@@ -0,0 +1,14 @@
+package ragunan.javafirst.ui.Main.Transaction.Detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransactionDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_detail);
+    }
+}
